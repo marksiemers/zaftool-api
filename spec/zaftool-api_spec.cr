ac9f0c3d08dce9644e2b5ddfdb7446c051f03797
Mark Siemers <mark.siemers@gmail.com>
2017-06-01 22:13:06 -0700

    Create kemal api with endpoints for searching

---
+++ spec/zaftool-api_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Zaftool::Api do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/zaftool-api/version.cr
@@ -0,0 +1,3 @@
+module Zaftool::Api
+  VERSION = "0.1.0"
+end
